8da32bd5898d2b1355ba8528f7eb5b9544632693
Alexandr Kuznetsov <memento_vivere@ukr.net>
2018-10-09 14:14:58 +0300

    Updates

---
+++ precache-manifest.7f5aecd224cb40e4f467ba0fce54a127.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "070bc6c6533697dda7a1",
+    "url": "/redux-user/static/js/runtime~main.070bc6c6.js"
+  },
+  {
+    "revision": "608f6af2685fddff31e8",
+    "url": "/redux-user/static/js/main.608f6af2.chunk.js"
+  },
+  {
+    "revision": "c53784ef1d8d885c506c",
+    "url": "/redux-user/static/js/1.c53784ef.chunk.js"
+  },
+  {
+    "revision": "608f6af2685fddff31e8",
+    "url": "/redux-user/static/css/main.8f20be2c.chunk.css"
+  },
+  {
+    "revision": "4c31b50b17c5899e6a26291d99749478",
+    "url": "/redux-user/index.html"
+  }
+];
